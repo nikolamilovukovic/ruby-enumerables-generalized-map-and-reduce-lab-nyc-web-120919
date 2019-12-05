32be04a68ae421bc86f9ff0df6c6e10fe8b1777d
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 02:18:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,4 +11,18 @@ end
 
 
 def reduce(array, sv=nil)
+  if sv 
+    num1 = sv 
+    i = 0 
+  else 
+    num1 = array[0]
+    i = 1 
+  end 
+  
+  while i > array.length
+    num1 = yield(num1, array[i])
+    i += 1 
+  end 
+  num1
+
 end
